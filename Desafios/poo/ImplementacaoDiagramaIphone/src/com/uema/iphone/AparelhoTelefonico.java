32f79f74d7a020d2486f236d7d8e4063a1c12506
Kiamisa <silas.comp04@gmail.com>
2024-08-07 17:40:24 -0300

    Implementação de desafio poo e reestruturação

---
+++ Desafios/poo/ImplementacaoDiagramaIphone/src/com/uema/iphone/AparelhoTelefonico.java
@@ -0,0 +1,7 @@
+package com.uema.iphone;
+
+public interface AparelhoTelefonico{
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
---
+++ Desafios/poo/ImplementacaoDiagramaIphone/src/com/uema/iphone/NavegadorInternet.java
@@ -0,0 +1,7 @@
+package com.uema.iphone;
+
+public interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
